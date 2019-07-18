39a2b4d8b4d08313ceb3e52dfa4498b60525feef
josephmuema <jmuema95@gmail.com>
2019-07-18 09:19:03 +0300

    updated the models

--- customer_details/models.py
+++ customer_details/models.py
@@ -1,28 +1,30 @@
-from django.db import models
+from django import models
 from django.contrib.auth.models import User
 
-Create your models here.
+# Create your models here.
 
 class Applicant(models.Models):
-  yourname = models.CharField(_('Yourname'), max_length=50)
+  yourname = models.CharField(max_length=50, blank=True)
   passport = models.CharField(_('ID/passport/inc/cert of REG No.'), max_length=50)
   pin = models.CharField(_('Pin No.'), max_length=50)
   p_o_box = models.CharField(_('P.O.Box '), max_length=50)
-  postalcode = = models.CharField(_('postalcode'), max_length=50)
-  city/town = models.CharField(_('city'),max_length=50))
-  physical/address = models.CharField(_('physical/address'), max_length=50)
+  postalcode = models.CharField(_('postalcode'), max_length=50)
+  city_town = models.CharField(max_length=50)
+  physical_address = models.CharField(_('physical/address'), max_length=50)
   mobile = models.CharField(_('mobile'), max_length=50)
-  home/officemobile = models.CharField(_('home/officemobile'), max_length=50)
+  home_officemobile = models.CharField(_('home/officemobile'), max_length=50)
   owner = models.CharField(_('owner'), max_length=50)
   tenant = models.CharField(_('tenant'), max_length=50)
   tp_o_box = models.CharField(_('tp.o.box'), max_length=50) 
   tpostalcode = models.CharField(_('tpostalcode'), max_length=50)
   tphoneno = models.CharField(_('tphoneno'), max_length=50)
-  business = models.CharField(_('business'), max_length=50)
+  business = models.ManyToManyField(_('business'), max_length=50)
   yrbusiness = models.CharField(_('yrbusiness'), max_length=50)
-  introby = models.CharField(_('introduced by'), max-length=50)
+  introby = models.CharField( max_length=50)
   purpose = models.CharField(_('purpose of assets being purchaced'), max_length=50)
 
+  def __str__(self):
+        return self.title
 
 class DealerSupplier(models.Models):
   dealername = models.CharField(_('DEALER NAME'),max_length=50)
@@ -30,6 +32,8 @@ class DealerSupplier(models.Models):
   telno = models.IntegerField(_('tel phone no'), max_length=50)
   invoiceno_date = models.CharField(_('invoice date'), max_length=50)
   salesperson = models.CharField(_('sales'), max_length=50)
+# def __str__(self):
+#         return self.title
 
 class OtherCredit(models.Models):
   name = models.CharField(_('Name'), max_length=50)
--- customer_details/views.py
+++ customer_details/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
-from uploads.core.models import Document
-from uploads.core.forms import DocumentForm
-
-
+# from uploads.core.models import Document
+# from uploads.core.forms import DocumentForm
+from .forms import ApplicantPostForm, DealerPostForm, DocumentForm
+from .models import Applicant,DealerSupplier,Document,OtherCredit
 # Create your views here.
+
+# def applicant_create_view(request):
+#     form = ApplicantPostForm()
+#     if form.is_valid():
+#         form.save()
+
+def welcome(request):
+    form = ApplicantPostForm()
+    return render(request,'forms.html',{"forms": forms})
+
+
+
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = DocumentForm()
+    return render(request, '', {
+        'form': form
+    }) 
