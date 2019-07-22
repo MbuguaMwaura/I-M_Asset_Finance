0573991f87ab20ecf9163579f65bd81604ce814b
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-07-22 14:10:02 +0300

    adds the credit activity

--- app/src/main/java/com/example/assetfinance/ui/AssetDetail.java
+++ app/src/main/java/com/example/assetfinance/ui/AssetDetail.java
@@ -1,5 +1,6 @@
 package com.example.assetfinance.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -56,7 +57,8 @@ public class AssetDetail extends AppCompatActivity implements View.OnClickListen
 
         }
         if (v == proceedSevenBtn){
-
+            Intent intent = new Intent(this,CreditActivity.class);
+            startActivity(intent);
         }
 
     }
---
+++ app/src/main/java/com/example/assetfinance/ui/CreditActivity.java
@@ -0,0 +1,44 @@
+package com.example.assetfinance.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.assetfinance.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CreditActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.name)
+    EditText name;
+    @BindView(R.id.facilityType) EditText facilityType;
+    @BindView(R.id.limit) EditText sanctionedLimit;
+    @BindView(R.id.outstanding) EditText outstanding;
+    @BindView(R.id.addCredit)
+    Button addCreditBtn;
+    @BindView(R.id.proceedEight) Button proceedEightBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credit);
+        setTitle("8. OTHER CREDIT FACILITIES AT I&M BANK LTD");
+        ButterKnife.bind(this);
+
+        addCreditBtn.setOnClickListener(this);
+        proceedEightBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == addCreditBtn){
+
+        }
+        if (v == proceedEightBtn){
+
+        }
+    }
+}
