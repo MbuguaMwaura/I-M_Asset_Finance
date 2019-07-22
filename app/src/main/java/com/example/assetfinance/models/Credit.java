ecd7e17563987c96c516bd05535956bc5ced3af4
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-07-22 14:12:40 +0300

    adds the credit classes to be able to create credit objects

---
+++ app/src/main/java/com/example/assetfinance/models/Credit.java
@@ -0,0 +1,48 @@
+package com.example.assetfinance.models;
+
+public class Credit {
+    String name;
+    String facilityType;
+    String sanctionedLimit;
+    String currentOutstanding;
+
+    public Credit(String name, String facilityType, String sanctionedLimit, String currentOutstanding) {
+        this.name = name;
+        this.facilityType = facilityType;
+        this.sanctionedLimit = sanctionedLimit;
+        this.currentOutstanding = currentOutstanding;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFacilityType() {
+        return facilityType;
+    }
+
+    public void setFacilityType(String facilityType) {
+        this.facilityType = facilityType;
+    }
+
+    public String getSanctionedLimit() {
+        return sanctionedLimit;
+    }
+
+    public void setSanctionedLimit(String sanctionedLimit) {
+        this.sanctionedLimit = sanctionedLimit;
+    }
+
+    public String getCurrentOutstanding() {
+        return currentOutstanding;
+    }
+
+    public void setCurrentOutstanding(String currentOutstanding) {
+        this.currentOutstanding = currentOutstanding;
+    }
+}
+
