5538f938e3fd3c75c916813dc0688cc7249d1170
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-07-25 10:05:39 +0300

    adds shared preferences to the data on the supplier activity

--- app/src/main/java/com/example/assetfinance/ui/SupplierActivity.java
+++ app/src/main/java/com/example/assetfinance/ui/SupplierActivity.java
@@ -1,12 +1,15 @@
 package com.example.assetfinance.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.assetfinance.Constants;
 import com.example.assetfinance.R;
 
 import butterknife.BindView;
@@ -22,6 +25,9 @@ public class SupplierActivity extends AppCompatActivity implements View.OnClickL
     @BindView(R.id.proceedSix)
     Button proceedSixBtn;
 
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,14 +35,72 @@ public class SupplierActivity extends AppCompatActivity implements View.OnClickL
         setTitle("6. DEALER/SUPPLIER");
         ButterKnife.bind(this);
 
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
+
+        String inputDealerName = mSharedPreferences.getString(Constants.SIX_DEALER_NAME, null);
+        String inputPostalAddress = mSharedPreferences.getString(Constants.SIX_POSTAL_ADDRESS_DEALER, null);
+        String inputTelNo = mSharedPreferences.getString(Constants.SIX_TEL_NO_DEALER, null);
+        String inputInvoiceDate = mSharedPreferences.getString(Constants.SIX_INVOICE_DATE, null);
+        String inputSalesPerson = mSharedPreferences.getString(Constants.SIX_SALES_PERSON, null);
+
+
+        if ((inputDealerName) != null){
+            dealerName.setText(inputDealerName);
+        }
+        if ((inputPostalAddress) != null){
+            postalAddress.setText(inputPostalAddress);
+        }
+        if ((inputTelNo)!= null){
+            number.setText(inputTelNo);
+        }
+        if ((inputInvoiceDate)!= null){
+            invoiceNoDate.setText(inputInvoiceDate);
+        }
+        if ((inputSalesPerson)!= null){
+            salesPerson.setText(inputSalesPerson);
+        }
+
         proceedSixBtn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         if (v == proceedSixBtn){
+            String inputDealerName = dealerName.getText().toString();
+            String inputPostalAddress = postalAddress.getText().toString();
+            String inputTelNo = number.getText().toString();
+            String inputSalesPerson = salesPerson.getText().toString();
+            String inputInvoiceDate = invoiceNoDate.getText().toString();
+
+            if (
+                    !(inputDealerName).equals("") &&
+                            !(inputPostalAddress).equals("") &&
+                            !(inputTelNo).equals("") &&
+                            !(inputSalesPerson).equals("") &&
+                            !(inputInvoiceDate).equals("")
+     ){
+                addToSharedPreferences(inputDealerName, inputPostalAddress, inputTelNo, inputSalesPerson, inputInvoiceDate);
+            }
+
             Intent intent = new Intent(this, AssetDetail.class);
             startActivity(intent);
         }
+    }
+    private void addToSharedPreferences(String inputDealerName,
+                                        String inputPostalAddress,
+                                        String inputTelNo,
+                                        String inputSalesPerson,
+                                        String inputInvoiceDate
+    ) {
+        mEditor.putString(Constants.SIX_DEALER_NAME,inputDealerName).apply();
+        mEditor.putString(Constants.SIX_POSTAL_ADDRESS_DEALER,inputPostalAddress).apply();
+        mEditor.putString(Constants.SIX_TEL_NO_DEALER, inputTelNo).apply();
+        mEditor.putString(Constants.SIX_SALES_PERSON, inputSalesPerson).apply();
+        mEditor.putString(Constants.SIX_INVOICE_DATE, inputInvoiceDate).apply();
+
+
+
     }
 }
