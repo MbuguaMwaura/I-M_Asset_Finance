d58b1e050ebe22b4d5ba2785d1def78dbfce9594
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-07-25 08:50:52 +0300

    adds shared preferences to the data on the application activity

--- app/src/main/java/com/example/assetfinance/ui/ApplicantActivity.java
+++ app/src/main/java/com/example/assetfinance/ui/ApplicantActivity.java
@@ -1,12 +1,15 @@
 package com.example.assetfinance.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.assetfinance.Constants;
 import com.example.assetfinance.R;
 
 import butterknife.BindView;
@@ -34,6 +37,8 @@ public class ApplicantActivity extends AppCompatActivity implements View.OnClick
     @BindView(R.id.proceedOne)
     Button proceedOne;
 
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +47,84 @@ public class ApplicantActivity extends AppCompatActivity implements View.OnClick
         setTitle("1. APPLICANT");
         ButterKnife.bind(this);
 
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
+
+        String inputName = mSharedPreferences.getString(Constants.ONE_NAME, null);
+        String inputIDCERT = mSharedPreferences.getString(Constants.ONE_ID_CERT, null);
+        String inputPin = mSharedPreferences.getString(Constants.ONE_PIN, null);
+        String inputPoBox = mSharedPreferences.getString(Constants.ONE_PO_BOX, null);
+        String inputPostalCode = mSharedPreferences.getString(Constants.ONE_POSTAL_CODE, null);
+        String inputLocation = mSharedPreferences.getString(Constants.ONE_LOCATION, null);
+        String inputNumber = mSharedPreferences.getString(Constants.ONE_PHONE_NUMBER, null);
+        String inputOfficeNumber = mSharedPreferences.getString(Constants.ONE_OFFICE_NUMBER, null);
+        String inputOwnerTenant = mSharedPreferences.getString(Constants.ONE_OWNER_TENANT, null);
+        String inputLandlord = mSharedPreferences.getString(Constants.ONE_TENTANT_LANDLORD, null);
+        String inputPoBoxLandLord = mSharedPreferences.getString(Constants.ONE_PO_BOX_LANDLORD, null);
+        String inputPostalCodeLandord = mSharedPreferences.getString(Constants.ONE_POSTAL_CODE_LANDLORD, null);
+        String inputNumberLandLord = mSharedPreferences.getString(Constants.ONE_PHONE_NUMBER_LANDLORD, null);
+        String inputBusiness = mSharedPreferences.getString(Constants.ONE_NATURE_OF_BUSINESS, null);
+        String inputYear = mSharedPreferences.getString(Constants.ONE_YEAR_BUSINESS, null);
+        String inputIntroBy = mSharedPreferences.getString(Constants.ONE_INTRO_BY, null);
+        String inputPurpose = mSharedPreferences.getString(Constants.ONE_PURPOSE, null);
+
+
+
+
+        if ((inputName) != null){
+            name.setText(inputName);
+        }
+        if ((inputIDCERT) != null){
+            idNumber.setText(inputIDCERT);
+        }
+        if ((inputPin)!= null){
+            pinNumber.setText(inputPin);
+        }
+        if ((inputPoBox)!= null){
+            poBox.setText(inputPoBox);
+        }
+        if ((inputPostalCode)!= null){
+            postalCode.setText(inputPostalCode);
+        }
+        if ((inputLocation)!= null){
+            location.setText(inputLocation);
+        }
+        if ((inputNumber)!= null){
+            mobileNumber.setText(inputNumber);
+        }
+        if ((inputOfficeNumber)!= null){
+            officeNumber.setText(inputOfficeNumber);
+        }
+        if ((inputOwnerTenant)!= null){
+            ownerTenant.setText(inputOwnerTenant);
+        }
+        if ((inputLandlord)!= null){
+            tenant.setText(inputLandlord);
+        }
+        if ((inputPoBoxLandLord)!= null){
+            poBoxTwo.setText(inputPoBoxLandLord);
+        }
+        if ((inputPostalCodeLandord)!= null){
+            postalCodeTwo.setText(inputPostalCodeLandord);
+        }
+        if ((inputNumberLandLord)!= null){
+            phoneNumber.setText(inputNumberLandLord);
+        }
+        if ((inputBusiness)!= null){
+            businessNature.setText(inputBusiness);
+        }
+        if ((inputYear)!= null){
+            yearOfBusiness.setText(inputYear);
+        }
+        if ((inputIntroBy)!= null){
+            introBy.setText(inputIntroBy);
+        }
+        if ((inputPurpose)!= null){
+            purpose.setText(inputPurpose);
+        }
+
+
+
 
         proceedOne.setOnClickListener(this);
     }
@@ -49,8 +132,93 @@ public class ApplicantActivity extends AppCompatActivity implements View.OnClick
     @Override
     public void onClick(View v) {
         if (v == proceedOne){
+            String inputName = name.getText().toString();
+            String inputIDCERT = idNumber.getText().toString();
+            String inputPin = pinNumber.getText().toString();
+            String inputPoBox = poBox.getText().toString();
+            String inputPostalCode = postalCode.getText().toString();
+            String inputLocation = location.getText().toString();
+            String inputNumber = mobileNumber.getText().toString();
+            String inputOfficeNumber = officeNumber.getText().toString();
+            String inputOwnerTenant = ownerTenant.getText().toString();
+            String inputLandlord = tenant.getText().toString();
+            String inputPoBoxLandLord = poBoxTwo.getText().toString();
+            String inputPostalCodeLandord = postalCodeTwo.getText().toString();
+            String inputNumberLandLord = phoneNumber.getText().toString();
+            String inputBusiness = businessNature.getText().toString();
+            String inputYear = yearOfBusiness.getText().toString();
+            String inputIntroBy = introBy.getText().toString();
+            String inputPurpose = purpose.getText().toString();
+
+            if (
+                    !(inputName).equals("") &&
+                            !(inputIDCERT).equals("") &&
+                            !(inputPin).equals("") &&
+                            !(inputPoBox).equals("") &&
+                            !(inputPostalCode).equals("") &&
+                            !(inputLocation).equals("") &&
+                            !(inputNumber).equals("") &&
+                            !(inputOfficeNumber).equals("") &&
+                            !(inputOwnerTenant).equals("") &&
+                            !(inputLandlord).equals("") &&
+                            !(inputPoBoxLandLord).equals("") &&
+                            !(inputPostalCodeLandord).equals("") &&
+                            !(inputNumberLandLord).equals("") &&
+                            !(inputBusiness).equals("") &&
+                            !(inputYear).equals("") &&
+                            !(inputIntroBy).equals("") &&
+                            !(inputPurpose).equals("")
+
+            ){
+                addToSharedPreferences(inputName, inputIDCERT, inputPin, inputPoBox, inputPostalCode,
+                       inputLocation, inputNumber, inputOfficeNumber, inputOwnerTenant, inputLandlord,
+                        inputPoBoxLandLord, inputPostalCodeLandord, inputNumberLandLord, inputBusiness,
+                        inputYear, inputIntroBy, inputPurpose);
+            }
+
+
             Intent intent = new Intent(this,BankDetailsActivity.class);
             startActivity(intent);
         }
+    }
+    private void addToSharedPreferences(String inputName,
+                                        String inputIDCERT,
+                                        String inputPin,
+                                        String inputPoBox,
+                                        String inputPostalCode,
+                                        String inputLocation,
+                                        String inputNumber,
+                                        String inputOfficeNumber,
+                                        String inputOwnerTenant,
+                                        String inputLandlord,
+                                        String inputPoBoxLandLord,
+                                        String inputPostalCodeLandord,
+                                        String inputNumberLandLord,
+                                        String inputBusiness,
+                                        String inputYear,
+                                        String inputIntroBy,
+                                        String inputPurpose) {
+        mEditor.putString(Constants.ONE_NAME,inputName).apply();
+        mEditor.putString(Constants.ONE_ID_CERT, inputIDCERT).apply();
+        mEditor.putString(Constants.ONE_PIN, inputPin).apply();
+        mEditor.putString(Constants.ONE_PO_BOX, inputPoBox).apply();
+        mEditor.putString(Constants.ONE_PO_BOX_LANDLORD, inputPoBoxLandLord).apply();
+        mEditor.putString(Constants.ONE_POSTAL_CODE, inputPostalCode).apply();
+        mEditor.putString(Constants.ONE_LOCATION, inputLocation).apply();
+        mEditor.putString(Constants.ONE_PHONE_NUMBER, inputNumber).apply();
+        mEditor.putString(Constants.ONE_OFFICE_NUMBER, inputOfficeNumber).apply();
+        mEditor.putString(Constants.ONE_OWNER_TENANT, inputOwnerTenant).apply();
+        mEditor.putString(Constants.ONE_TENTANT_LANDLORD, inputLandlord).apply();
+        mEditor.putString(Constants.ONE_POSTAL_CODE_LANDLORD, inputPostalCodeLandord).apply();
+        mEditor.putString(Constants.ONE_PHONE_NUMBER_LANDLORD, inputNumberLandLord).apply();
+        mEditor.putString(Constants.ONE_NATURE_OF_BUSINESS, inputBusiness).apply();
+        mEditor.putString(Constants.ONE_YEAR_BUSINESS, inputYear).apply();
+        mEditor.putString(Constants.ONE_INTRO_BY, inputIntroBy).apply();
+        mEditor.putString(Constants.ONE_PURPOSE, inputPurpose).apply();
+
+
+
+
+
     }
 }
